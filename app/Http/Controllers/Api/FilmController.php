30b8a8512f4add8fba36b10c43e6c6a4c09eaeb6
deas.faras@gmail.com <deas.faras@gmail.com>
2024-07-26 14:48:54 +0700

    Api

--- app/Http/Controllers/Api/FilmController.php
+++ app/Http/Controllers/Api/FilmController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\Film;
+use App\Models\film;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Validator;
